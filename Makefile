786af6c91581b96e0358d9832c99908644245599
Lieselotte <52315535+she3py@users.noreply.github.com>
2024-05-13 16:43:03 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,16 @@
+CC = gcc
+CFLAGS = -Wpedantic -Wall -Wextra -fanalyzer -g -Iglad/include
+LFLAGS = -lglfw
+
+physarum: out/gl.o out/main.o
+	gcc $(CFLAGS) $(LFLAGS) $^ -o $@
+
+out/main.o: src/main.c
+	gcc $(CFLAGS) -c $< -o $@
+
+out/gl.o: glad/src/gl.c
+	gcc $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -rf out/*
+
---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    if(!glfwInit()) {
+        return -1;
+    }
+    
+    GLFWwindow *window = glfwCreateWindow(720, 480, "Hello World", NULL, NULL);
+    if(!window) {
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+    
+    while(!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
