8e4bf5ada49157b867cf9ee68cf1cff2ab99da96
Lieselotte <52315535+she3py@users.noreply.github.com>
2024-07-17 14:55:26 +0200

    Publish physarum

--- publish.sh
+++ publish.sh
@@ -5,15 +5,18 @@ cd  $(dirname -- $(readlink --canonicalize-existing -- $BASH_SOURCE))
 
 make -B wasm
 
-cp src/common/passthrough.vsh src/www/common-passthrough.vsh
+cp src/www/* generated/
+cp src/common/passthrough.vsh generated/common-passthrough.vsh
 
-wasm2wat physarum --inline-exports --generate-names -o src/www/physarum.wat
-mv physarum src/www/physarum.wasm
+wasm2wat physarum --inline-exports --generate-names -o generated/physarum.wat
+mv physarum generated/physarum.wasm
 
-cp src/physarum/config.c src/www/physarum-config.c
-cp src/physarum/simulator.fsh src/www/physarum-simulator.fsh
-cp src/physarum/renderer.fsh src/www/physarum-renderer.fsh
+cp src/physarum/config.c generated/physarum-config.c
+cp src/physarum/simulator.fsh generated/physarum-simulator.fsh
+cp src/physarum/renderer.fsh generated/physarum-renderer.fsh
 
+cd generated/
 tree -P physarum.html -T ALife -H . -o index.html
+cd ..
 
-git subtree push --prefix src/www/ origin gh-pages
+git subtree push --prefix generated/ origin gh-pages
